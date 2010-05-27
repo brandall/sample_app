da9bf304f8668e5e413b440df6cdc80e51e611eb
Brian Randall <brian.randall@gmail.com>
2010-05-27 12:19:26 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.about   '/about',   :controller => 'pages', :action => 'about'
+  map.help    '/help',    :controller => 'pages', :action => 'help'
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -17,7 +22,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Sample resource route with sub-resources:
   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
+   
   # Sample resource route with more complex sub-resources
   #   map.resources :products do |products|
   #     products.resources :comments
@@ -31,7 +36,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => 'pages', :action => 'home'
 
   # See how all your routes lay out with "rake routes"
 
